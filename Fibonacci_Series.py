850b720b4af1ed04b342a9341574fc67e8cf1b22
CoderDS76 <71945305+CoderDS76@users.noreply.github.com>
2022-11-17 02:16:28 +0530

    commit

--- Fibonacci_Series.py
+++ Fibonacci_Series.py
@@ -2,9 +2,14 @@ n=int(input("Enter the number of times to iterate:\t"))
 num1=0
 num2=1
 num=0
-print(num1,num2,end=" ")
-for i in range(1,n+1):
-    num=num1+num2
-    print(num,end=" ")
-    num1=num2
-    num2=num
+if n>2:
+    print(num1,num2,end=" ")
+    for i in range(1,n-1):
+        num=num1+num2
+        print(num,end=" ")
+        num1=num2
+        num2=num
+elif n==2:
+    print(num1,num2)
+else:
+    print(num1)
